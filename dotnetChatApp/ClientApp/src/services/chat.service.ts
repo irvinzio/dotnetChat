dcfb55443dedeeb6acb68751977d772d4768f951
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 17:11:48 -0600

    chat service

---
+++ dotnetChatApp/ClientApp/src/services/chat.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { MessageRequest } from 'src/core/models/messageRequest';
+import * as signalR from '@microsoft/signalr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+   private  connection: any = new signalR.HubConnectionBuilder().withUrl("https://localhost:44379/chatsocket")   // mapping to the chathub as in startup.cs
+                                         .configureLogging(signalR.LogLevel.Information)
+                                         .build();
+   readonly POST_URL = "https://localhost:44379/api/chat/send"
+
+  private receivedMessageObject: MessageRequest = new MessageRequest();
+  private sharedObj = new Subject<MessageRequest>();
+
+  constructor(private http: HttpClient) { 
+    this.connection.onclose(async () => {
+      await this.start();
+    });
+   this.connection.on("ReceiveOne", (user, message) => { this.mapReceivedMessage(user, message); });
+   this.start();                 
+  }
+
+
+  // Strart the connection
+  public async start() {
+    try {
+      await this.connection.start();
+      console.log("connected");
+    } catch (err) {
+      console.log(err);
+      setTimeout(() => this.start(), 5000);
+    } 
+  }
+
+  private mapReceivedMessage(user: string, message: string): void {
+    this.receivedMessageObject.user = user;
+    this.receivedMessageObject.msgText = message;
+    this.sharedObj.next(this.receivedMessageObject);
+ }
+
+  public broadcastMessage(msgDto: any) {
+    this.http.post(this.POST_URL, msgDto).subscribe(data => console.log(data));
+    // this.connection.invoke("SendMessage1", msgDto.user, msgDto.msgText).catch(err => console.error(err));    // This can invoke the server method named as "SendMethod1" directly.
+  }
+
+  public retrieveMappedObject(): Observable<MessageRequest> {
+    return this.sharedObj.asObservable();
+  }
+
+
+}
