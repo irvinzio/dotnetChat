b0bfbc902d83374ce697b75d34b84b8a96db7294
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 20:39:11 -0600

    create login - logout endpoints

--- dotnetChatApp/Controllers/SecurityController.cs
+++ dotnetChatApp/Controllers/SecurityController.cs
@@ -1,18 +1,32 @@
 ﻿using DotnetChat.Core.Interfaces;
+using DotnetChat.Core.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace dotnetChatApp.Controllers
 {
-    public class SecurityController : Controller
+    [Route("api/security")]
+    [ApiController]
+    public class SecurityController : ControllerBase
     {
         private readonly ISecurityService _securityService;
         public SecurityController(ISecurityService securityService)
         {
             _securityService = securityService;
+        } 
+        [Route("login")]
+        [HttpPost]
+        public async Task<IActionResult> login([FromBody] LogInRequest loginRequest)
+        {
+            var user = await _securityService.LogIn(loginRequest);
+            return Ok(user);
+        }
+        [Route("logout")]
+        [HttpPost]
+        public async Task<IActionResult> logout([FromBody] LogInRequest loginRequest)
+        {
+            await _securityService.LogOut();
+            return NoContent();
         }
     }
 }
