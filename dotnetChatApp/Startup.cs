0de40b9012de1b6dd03b186cff7c99d54cc34501
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 20:38:02 -0600

    add swagger doc

--- dotnetChatApp/Startup.cs
+++ dotnetChatApp/Startup.cs
@@ -1,11 +1,12 @@
 using dotnetChatApp.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace dotnetChatApp
 {
@@ -22,6 +23,22 @@ namespace dotnetChatApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Dotnet Chat API",
+                    Description = "API to interact with dotnetChat",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Irving Ramirez",
+                        Email = "Irvinzio.ram@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/irving-ramirez-847395a5/"),
+                    }
+                });
+            });
             services.AddSignalR(); 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -50,7 +67,16 @@ namespace dotnetChatApp
             {
                 app.UseSpaStaticFiles();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
