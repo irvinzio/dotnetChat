e639db6e1ba587db50195a9b9493aa0599e76a7c
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 19:58:50 -0600

    user message service

--- dotnetChatApp/Controllers/ChatController.cs
+++ dotnetChatApp/Controllers/ChatController.cs
@@ -1,7 +1,9 @@
-using dotnetChatApp.Core.Models;
+using DotnetChat.Core.Models;
+using DotnetChat.Service;
 using dotnetChatApp.Hubs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
 namespace dotnetChatApp.Controllers
 {
@@ -10,17 +12,20 @@ namespace dotnetChatApp.Controllers
     public class ChatController : ControllerBase
     {
         private readonly IHubContext<ChatHub> _hubContext;
+        private readonly ChatService _chatService;
 
-        public ChatController(IHubContext<ChatHub> hubContext)
+        public ChatController(IHubContext<ChatHub> hubContext, ChatService chatService)
         {
             _hubContext = hubContext;
+            _chatService = chatService;
         }
 
         [Route("send")]
         [HttpPost]
-        public IActionResult SendRequest([FromBody] MessageRequest msg)
+        public async Task<IActionResult> SendRequest([FromBody] MessageRequest message)
         {
-            _hubContext.Clients.All.SendAsync("ReceiveMessage", msg.User, msg.Text);
+            await _chatService.SaveMessage(message);
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", message.Alias, message.Text);
             return Ok();
         }
     }
