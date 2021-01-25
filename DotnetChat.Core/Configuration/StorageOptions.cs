22ee516eb951c76e046438758438687c223b83fa
irvinzio <irvinzio.ram@gmail.com>
2021-01-25 16:50:33 -0600

    storage Ioptions

---
+++ DotnetChat.Core/Configuration/StorageOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetChat.Core.Configuration
+{
+    public class StorageOptions
+    {
+        public string ConnectonString { get; set; }
+    }
+}
