0575186310aa5230c9178b724c62d4b9e688fb70
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 19:57:41 -0600

    create userService

---
+++ DotnetChat.Service/ChatService.cs
@@ -0,0 +1,18 @@
+﻿using DotnetChat.Core.Interfaces;
+using DotnetChat.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetChat.Service
+{
+
+    public class ChatService : IChatService
+    {
+        public Task<MessageResponse> SaveMessage(MessageRequest message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
