d217fe04e01a4d47557442e48ed0b01632d112ec
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 21:13:22 -0600

    repository interface

---
+++ DotnetChat.Core/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DotnetChat.Core.Interfaces.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> Get();
+        IEnumerable<T> Get(Expression<Func<T, bool>> predicate);
+        T Get(Guid id);
+        T Add(T entity);
+        T Update(T entity);
+        T Delete(Guid id);
+    }
+}
