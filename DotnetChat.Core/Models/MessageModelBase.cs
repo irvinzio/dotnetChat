326a935756e8dfb10aa142922eacade5701e6535
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 20:44:21 -0600

    cahnge text to string on message model

--- DotnetChat.Core/Models/MessageModelBase.cs
+++ DotnetChat.Core/Models/MessageModelBase.cs
@@ -4,7 +4,6 @@ namespace DotnetChat.Core.Models
 {
     public abstract class MessageModelBase
     {
-        public Guid Text { get; set; }
-        public Guid UserId { get; set; }
+        public string Text { get; set; }
     }
 }
