4db8a061acdc68ad239390b43bcd4d7ec12ec952
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 18:28:11 -0600

    creaete chat entities

---
+++ DotnetChat.Data/Entities/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotnetChat.Data.Entities
+{
+    public class Message
+    { 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid MessageId { get; set; }
+        public Guid Text { get; set; }
+        public User UserId { get; set; }
+    }
+}
---
+++ DotnetChat.Data/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotnetChat.Data.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid UserId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Alias { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+        
+        public IEnumerable<Messages> Messages { get; set; }
+    }
+}
