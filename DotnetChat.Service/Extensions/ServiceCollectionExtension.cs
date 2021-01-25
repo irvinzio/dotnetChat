e65c56ed7971aec4a94a183883c453c9cd93c18f
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 19:57:59 -0600

    service registration extension

---
+++ DotnetChat.Service/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using DotnetChat.Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotnetChat.Service.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IChatService, ChatService>();
+            return services;
+        }
+    }
+}
