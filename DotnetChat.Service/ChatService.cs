77dec0273d56f36898951de78295b543d00fc4e6
irvinzio <irvinzio.ram@gmail.com>
2021-01-25 16:51:38 -0600

    call repsoitoryso save message

--- DotnetChat.Service/ChatService.cs
+++ DotnetChat.Service/ChatService.cs
@@ -1,5 +1,9 @@
-﻿using DotnetChat.Core.Interfaces;
+﻿using AutoMapper;
+using DotnetChat.Core.Interfaces;
+using DotnetChat.Core.Interfaces.Repositories;
 using DotnetChat.Core.Models;
+using DotnetChat.Data.Entities;
+using DotnetChat.Infrastructure.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,12 +11,19 @@ using System.Threading.Tasks;
 
 namespace DotnetChat.Service
 {
-
     public class ChatService : IChatService
     {
-        public Task<MessageResponse> SaveMessage(MessageRequest message)
+        private readonly IRepository<Message> _messageRepo;
+        private readonly IMapper _mapper;
+        public ChatService(IRepository<Message> repository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _messageRepo  = repository;
+            _mapper = mapper;
+        }
+        public async Task<MessageResponse> SaveMessage(MessageRequest message)
+        {
+            var messageEntity =  _mapper.Map<Message>(message);
+            return _mapper.Map<MessageResponse>(await _messageRepo.Update(messageEntity));
         }
     }
 }
