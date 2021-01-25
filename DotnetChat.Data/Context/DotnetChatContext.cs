5c07c1a3731be049aaf63c6d2bf470b307a5a386
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 18:28:27 -0600

    create chat db context

---
+++ DotnetChat.Data/Context/DotnetChatContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using DotnetChat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetChat.Data.Context
+{
+    public class DotnetChatContext : DbContext
+    {
+        public DotnetChatContext(DbContextOptions<DotnetChatContext> options)
+        : base(options)
+        { }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
