ce5724d2b9bf95e0ba367fc5ec70214ca5f6c67f
irvinzio <irvinzio.ram@gmail.com>
2021-01-25 16:49:35 -0600

    add services mapping

--- DotnetChat.Service/Extensions/ServiceCollectionExtension.cs
+++ DotnetChat.Service/Extensions/ServiceCollectionExtension.cs
@@ -1,12 +1,19 @@
-﻿using DotnetChat.Core.Interfaces;
+﻿using AutoMapper;
+using DotnetChat.Core.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DotnetChat.Service.Extensions
 {
     public static class ServiceCollectionExtension
     {
+        public static IServiceCollection AddRepositoryMappings(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Mapping.DataModelMappingProfile));
+            return services;
+        }
         public static IServiceCollection RegisterServices(this IServiceCollection services)
         {
+            services.AddRepositoryMappings();
             services.AddScoped<IChatService, ChatService>();
             services.AddScoped<ISecurityService, SecurityService>();
             return services;
---
+++ DotnetChat.Service/Mapping/DataModelMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DotnetChat.Core.Models;
+using DotnetChat.Data.Entities;
+
+namespace DotnetChat.Service.Mapping
+{
+    public class DataModelMappingProfile : Profile
+    {
+        public DataModelMappingProfile()
+        {
+            CreateMap<MessageRequest, Message>().ReverseMap();
+            CreateMap<MessageResponse, Message>().ReverseMap();
+            CreateMap<UserRequest, User>().ReverseMap();
+            CreateMap<UserResponse, User>().ReverseMap();
+        }
+    }
+}
