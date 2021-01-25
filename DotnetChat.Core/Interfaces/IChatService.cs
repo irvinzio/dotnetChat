cfbc276022779512fc4a3bfb3ef5c09fa85b2eb0
irvinzio <irvinzio.ram@gmail.com>
2021-01-24 19:55:40 -0600

    add chat interface

---
+++ DotnetChat.Core/Interfaces/IChatService.cs
@@ -0,0 +1,10 @@
+﻿using DotnetChat.Core.Models;
+using System.Threading.Tasks;
+
+namespace DotnetChat.Core.Interfaces
+{
+    public interface IChatService
+    {
+        Task<MessageResponse> SaveMessage(MessageRequest message);
+    }
+}
