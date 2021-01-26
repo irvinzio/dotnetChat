2349f118d34a1258117d0946bb73fbe7ffe71a01
irvinzio <irvinzio.ram@gmail.com>
2021-01-26 02:13:29 -0600

    remove commented not used code

--- DotnetChat.Infrasctructure/Extensions/SwaggerExtension.cs
+++ DotnetChat.Infrasctructure/Extensions/SwaggerExtension.cs
@@ -24,27 +24,19 @@ namespace DotnetChat.Infrasctructure.Extensions
                         Url = new Uri("https://www.linkedin.com/in/irving-ramirez-847395a5/"),
                     }
                 });
-                //c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                //{
-                //    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: {token}\"",
-                //    Name = "Authorization",
-                //    In = ParameterLocation.Header,
-                //    Type = SecuritySchemeType.ApiKey
-                //});
-                //First we define the security scheme
                 c.AddSecurityDefinition("Bearer", //Name the security scheme
                     new OpenApiSecurityScheme
                     {
                         Description = "JWT Authorization header using the Bearer scheme.",
-                        Type = SecuritySchemeType.Http, //We set the scheme type to http since we're using bearer authentication
-                        Scheme = "bearer" //The name of the HTTP Authorization scheme to be used in the Authorization header. In this case "bearer".
+                        Type = SecuritySchemeType.Http, 
+                        Scheme = "bearer"
                     });
 
                 c.AddSecurityRequirement(new OpenApiSecurityRequirement{
                     {
                         new OpenApiSecurityScheme{
                             Reference = new OpenApiReference{
-                                Id = "Bearer", //The name of the previously defined security scheme.
+                                Id = "Bearer", 
                                 Type = ReferenceType.SecurityScheme
                             }
                         },new List<string>()
