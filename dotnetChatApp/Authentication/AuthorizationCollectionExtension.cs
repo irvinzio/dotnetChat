cc5af10b8b21fa228239dd031860fb3cbdae212d
irvinzio <irvinzio.ram@gmail.com>
2021-01-26 02:19:38 -0600

    abtraction of jwt authorization service config

---
+++ dotnetChatApp/Authentication/AuthorizationCollectionExtension.cs
@@ -0,0 +1,48 @@
+﻿using DotnetChat.Data.Context;
+using DotnetChat.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace dotnetChatApp.Authentication
+{
+    public static class AuthorizationCollectionExtension
+   {
+       public static IServiceCollection ConfigureIdentity(this IServiceCollection services)
+       {
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<DotnetChatContext>()
+                .AddDefaultTokenProviders();
+            return services;
+       }
+       public static IServiceCollection ConfigureAuthentication(this IServiceCollection services, IConfiguration Configuration)
+       {
+            // Adding Authentication  
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            // Adding Jwt Bearer  
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["JWT:ValidAudience"],
+                    ValidIssuer = Configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                };
+            });
+            return services;
+       }
+   }
+}
--- dotnetChatApp/Startup.cs
+++ dotnetChatApp/Startup.cs
@@ -1,19 +1,14 @@
-using DotnetChat.Data.Context;
-using DotnetChat.Data.Entities;
 using DotnetChat.Infrasctructure.Extensions;
 using DotnetChat.Infrasctructure.Extensions.Initialization;
 using DotnetChat.Service.Extensions;
+using dotnetChatApp.Authentication;
 using dotnetChatApp.Hubs;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace dotnetChatApp
 {
@@ -36,32 +31,8 @@ namespace dotnetChatApp
             services.RegisterInfrastructureServices();
             services.ConfigureSwagger();
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<DotnetChatContext>()
-                .AddDefaultTokenProviders();
-
-            // Adding Authentication  
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-
-            // Adding Jwt Bearer  
-            .AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.RequireHttpsMetadata = false;
-                options.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidAudience = Configuration["JWT:ValidAudience"],
-                    ValidIssuer = Configuration["JWT:ValidIssuer"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
-                };
-            });
+            services.ConfigureIdentity();
+            services.ConfigureAuthentication(Configuration);
 
             services.AddSignalR(); 
             // In production, the Angular files will be served from this directory
