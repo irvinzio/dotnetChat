98c1b8d9763e3f98e7410a5628fa73f041a7bb2e
irvinzio <irvinzio.ram@gmail.com>
2021-01-26 22:14:41 -0600

    create stock provide as subtitute of bot

---
+++ DotnetChat.Infrasctructure/StockProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace DotnetChat.Infrasctructure
+{
+    public interface IStockProvider
+    {
+        string GetStocksInfo(string stock);
+    }
+    public class StockProvider : IStockProvider
+    {
+        private const string STOCK_BASE_URL = "https://stooq.com";
+        private string _queryParams;
+        private string _fullStockUrl;
+
+        private void SetUrl(string stock)
+        {
+            _queryParams = $"/q/l/?s={stock}&f=sd2t2ohlcv&h&e=csv";
+            _fullStockUrl = $"{STOCK_BASE_URL}{_queryParams}";
+        }
+        public string GetStocksInfo(string stock)
+        {
+            SetUrl(stock);
+            var stockInfo = String.Empty;
+            var req = (HttpWebRequest)HttpWebRequest.Create(_fullStockUrl);
+            using (var resp = req.GetResponse())
+            {
+                using (var stream = resp.GetResponseStream())
+                {
+                    var reader = new StreamReader(stream);
+                    stockInfo = reader.ReadToEnd();
+                }
+            }
+            return stockInfo;
+        }
+    }
+}
