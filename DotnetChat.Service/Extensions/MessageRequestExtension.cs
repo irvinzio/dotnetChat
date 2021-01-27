eec9ea23c39e1074718f8cc42760dc5d8d6bfabb
irvinzio <irvinzio.ram@gmail.com>
2021-01-26 22:15:06 -0600

    create extension to calidate comand

---
+++ DotnetChat.Service/Extensions/MessageRequestExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DotnetChat.Service.Extensions
+{
+    public static class MessageRequestExtension
+    {
+        public static bool IsTextComand(this string text)
+        {
+            var stockRegex = @"\/stock=[a-z _ A-Z 0-9]+";
+            Regex res = new Regex(stockRegex);
+            if (res.IsMatch(text))
+                return true;
+            else
+                return false;
+        }
+    }
+}
